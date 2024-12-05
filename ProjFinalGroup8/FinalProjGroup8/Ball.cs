289c042667ddc4da5a5325bdc0a6373510850a98
Kunj Lakhani <Klakhani0551@conestogac.on.ca>
2024-12-05 15:18:20 -0500

    adding ball and bar class

---
+++ ProjFinalGroup8/FinalProjGroup8/Ball.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Input;
+using System.Threading.Tasks;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace Project1
+{
+	public class Ball
+	{
+		public Vector2 Position;
+		public Vector2 Velocity;
+		private Texture2D texture;
+		private int speed = 6;
+		private int radius = 10;
+
+		public Ball(Texture2D ballTexture, Vector2 startPosition)
+		{
+			texture = ballTexture;
+			Position = startPosition;
+			Velocity = new Vector2(speed, -speed);
+		}
+
+		public void Update()
+		{
+			Position += Velocity;
+
+
+			if (Position.X <= 0 || Position.X >= 1200 - texture.Width)
+			{
+				Velocity.X = -Velocity.X;
+			}
+
+			if (Position.Y <= 0)
+			{
+				Velocity.Y = -Velocity.Y;
+			}
+		}
+
+		public void ReverseYDirection()
+		{
+			Velocity.Y = -Velocity.Y;
+		}
+
+		public Rectangle GetBounds()
+		{
+			return new Microsoft.Xna.Framework.Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(texture, Position, Microsoft.Xna.Framework.Color.White);
+		}
+	}
+}
---
+++ ProjFinalGroup8/FinalProjGroup8/Bar.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+	public class Bar
+	{
+		public Vector2 Position;
+		public int Speed = 10;
+		private int width = 300;
+		private int height = 20;
+
+		private Texture2D texture;
+
+		public Bar(Texture2D barTexture, int screenWidth)
+		{
+			texture = barTexture;
+			Position = new Vector2(screenWidth / 2 - width / 2, 800); // Bar at the bottom
+		}
+
+		public void Update(int screenWidth)
+		{
+			KeyboardState state = Keyboard.GetState();
+
+			if (state.IsKeyDown(Keys.Left))
+			{
+				Position.X -= Speed;
+			}
+			if (state.IsKeyDown(Keys.Right))
+			{
+				Position.X += Speed;
+			}
+
+			// Ensure the bar stays within screen bounds
+			Position.X = MathHelper.Clamp(Position.X, 0, screenWidth - width);
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(texture, Position, Color.White);
+		}
+
+		public Rectangle GetBounds()
+		{
+			return new Rectangle((int)Position.X, (int)Position.Y, width, height);
+		}
+	}
+}
